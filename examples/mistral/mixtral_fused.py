29da958f75f971e8a43d24927c091fc34f1ee3b2
Crystalcareai <162942000+Crystalcareai@users.noreply.github.com>
2024-03-18 12:23:05 -0500

    Update mixtral_fused.py

--- examples/mistral/mixtral_fused.py
+++ examples/mistral/mixtral_fused.py
@@ -1,8 +1,6 @@
 import torch
-from tqdm import tqdm
-from axolotl.monkeypatch.moe.moe import SparseMoeBlock
+from axolotl.monkeypatch.axolotl_mixtral_moe_patch import patch_model
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
-from transformers.models.mixtral.modeling_mixtral import MixtralSparseMoeBlock
 
 def compute_memory_used_pct(device):
     memory_used = torch.cuda.max_memory_allocated(device) / (1024**3)
@@ -16,23 +14,8 @@ def compute_memory_used_pct(device):
 model_path = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 # Load model
-model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
-modules = {k:v for k,v in model.named_modules() if isinstance(v, MixtralSparseMoeBlock)}
-
-with tqdm(modules.items(), desc="scatter moe") as pbar:
-    for name, module in pbar:
-        smoe = SparseMoeBlock(
-            experts=module.experts,
-            gate=module.gate,
-            hidden_dim=module.hidden_dim,
-            ffn_dim=module.ffn_dim,
-            num_experts=module.num_experts,
-            top_k=module.top_k,
-        )
-        setattr(model, name, smoe)
-        for device_index in range(torch.cuda.device_count()):
-            device_memory_pct = compute_memory_used_pct(device_index)
-            print(device_index, device_memory_pct)
+model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto", load_in_4bit=False)
+patch_model(model)
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
